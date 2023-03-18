8e8759b2e5500205675644c5b8af49ffef9dae92
Enes Kaan Bilir <bilireneskaan@gmail.com>
2023-03-18 15:40:15 +0300

    feat:ract temel komutları tekrar edildi

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./index.css"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <section>
-    <p>Hello World !!</p>
+  <section style={{border:"3px solid black", padding:"1rem"}} className='container'>
+    <p className='kirmizi-yazi'>Hello World !!</p>
   </section>
 
 );
 
+ 
